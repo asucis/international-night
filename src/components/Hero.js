4e0063a728729f201eb2bf9cce8bcd3ffb88c0b7
dickwyn <dickwyn@outlook.com>
2019-10-30 21:37:34 -0700

    (feat) introduce new hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDoubleDown } from "@fortawesome/free-solid-svg-icons";
+import Countdown from "./Countdown";
+import Image from "./Image";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-logo">
+        <Image />
+      </div>
+      <Countdown />
+      <div className="information">
+        <div className="section-one">
+          <a
+            className="button"
+            href="https://www.eiseverywhere.com/ereg/newreg.php?eventid=495034"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Get Free Tickets
+          </a>
+        </div>
+        <div className="section-two">
+          <a className="more-arrow bounce" href="#content">
+            <FontAwesomeIcon icon={faAngleDoubleDown} size="2x" color="#fff" />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,16 @@
 import React from "react";
-import Countdown from "../components/Countdown";
-import Image from "../components/Image";
-import Layout from "../components/layout";
+import Hero from "../components/Hero";
 import SEO from "../components/seo";
 
 const IndexPage = () => {
   return (
-    <Layout>
+    <>
       <SEO title="International Night 2019" />
-      <div className="hero-logo">
-        <Image />
-      </div>
-      <Countdown />
-      <div className="information">
-        <p className="location">SDFC Fields - ASU Tempe</p>
-        <p>Friday, November 15</p>
-        <p>Activites begin @ 6pm</p>
-        <a
-          className="button"
-          href="https://www.eiseverywhere.com/ereg/newreg.php?eventid=495034"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Get Free Tickets
-        </a>
-      </div>
-    </Layout>
+      <Hero />
+      <section id="content">
+        <h1>Hello</h1>
+      </section>
+    </>
   );
 };
 
