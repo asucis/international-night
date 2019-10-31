2d3df695fb353719cea0eb984e84fd48a0652502
dickwyn <dickwyn@outlook.com>
2019-10-30 22:58:20 -0700

    (style) temp content section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Footer from "../components/Footer";
 import SEO from "../components/seo";
 
 const IndexPage = () => {
@@ -8,8 +9,23 @@ const IndexPage = () => {
       <SEO title="International Night 2019" />
       <Hero />
       <section id="content">
-        <h1>Hello</h1>
+        <center>
+          <h1>International Night</h1>
+          <h3>SDFC FIELDS - ASU TEMPE</h3>
+          <h3>FRIDAY, NOVEMBER 15</h3>
+          <h3>ACTIVITES BEGIN @ 6PM</h3>
+          <h3>
+            <span role="img" aria-label="alert">
+              ⚠️
+            </span>{" "}
+            This event adheres to the clear bag policy{" "}
+            <span role="img" aria-label="alert">
+              ⚠️
+            </span>
+          </h3>
+        </center>
       </section>
+      <Footer />
     </>
   );
 };
