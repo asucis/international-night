40f8503a4e024156cbea4c17e4cad35436f46cff
dickwyn <dickwyn@outlook.com>
2019-10-30 23:15:07 -0700

    (fix) import css styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
 import SEO from "../components/seo";
+import "../scss/App.scss";
 
 const IndexPage = () => {
   return (
