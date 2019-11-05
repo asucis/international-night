2dc8173b72daec7a422329be8b1af37ccf024ca9
dickwyn <dickwyn@outlook.com>
2019-11-04 22:09:21 -0700

    (feat) style event details with icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,10 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMapMarkedAlt,
+  faTicketAlt,
+  faExclamationCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
 import SEO from "../components/seo";
@@ -12,31 +18,60 @@ const IndexPage = () => {
       <SEO title="International Night 2019" />
       <Hero />
       <section id="content">
-        <div className="event-info">
-          <h1>International Night</h1>
-          <h2>
-            Join us for a night full of festivities, games, giveaways,
-            performances, and everyone&apos;s favorite, food! Immerse yourself
-            in the unique multicultural society here at ASU, fostered by our
-            community of 10,000+ international students. Best of all, you can
-            experience the beauty of the world first-hand right in your
-            backyard!
-          </h2>
-          <h3>SDFC FIELDS - ASU TEMPE</h3>
-          <h3>FRIDAY, NOVEMBER 15</h3>
-          <h3>ACTIVITES BEGIN @ 6PM</h3>
-          <h3>
-            <span role="img" aria-label="alert">
-              ⚠️
-            </span>{" "}
-            This event adheres to the clear bag policy{" "}
-            <span role="img" aria-label="alert">
-              ⚠️
-            </span>
-          </h3>
-        </div>
+        <div className="content-container">
+          <div className="event-info">
+            <h1>International Night</h1>
+            <h2>
+              Join us for a night full of festivities, games, giveaways,
+              performances, and everyone&apos;s favorite, food! Immerse yourself
+              in the unique multicultural society here at ASU, fostered by our
+              community of 10,000+ international students. Best of all, you can
+              experience the beauty of the world first-hand right in your
+              backyard!
+            </h2>
+            <div className="key-information-container">
+              <div className="key-information">
+                <FontAwesomeIcon icon={faMapMarkedAlt} size="6x" />
+                <h3>SDFC Intramural Fields</h3>
+                <h3>ASU Tempe Campus</h3>
+                <a
+                  href="https://goo.gl/maps/41xVrzBUcq4zoLMv5"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View in map
+                </a>
+              </div>
+              <div className="key-information">
+                <FontAwesomeIcon icon={faTicketAlt} size="6x" />
+                <h3>Ticket required at entry</h3>
+                <a
+                  href="https://signup.asucis.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Claim your free tickets
+                </a>
+              </div>
+              <div className="key-information">
+                <FontAwesomeIcon icon={faExclamationCircle} size="6x" />
+                <h3>
+                  For your safety, this event adheres to the Sun Devil Athletics
+                  clear bag policy
+                </h3>
+                <a
+                  href="https://thesundevils.com/sports/2018/8/9/clear-bag-policy.aspx"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn more
+                </a>
+              </div>
+            </div>
+          </div>
 
-        <Performances />
+          <Performances />
+        </div>
       </section>
       <Footer />
     </>
