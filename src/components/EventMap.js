55b0419bde41069e7fd5a95d33126f0d3596156a
dickwyn <dickwyn@outlook.com>
2019-11-12 20:31:55 -0700

    (feat) load image from CMS json file

--- src/components/EventMap.js
+++ src/components/EventMap.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-console */
 import React from "react";
 import Image from "./Image";
+import { eventMapImage } from "../../data/eventMap.json";
 
 const EventMap = () => (
   <>
     <h1 className="section-header">Event Map</h1>
-    <Image fileName="event-map.png" />
+    <Image fileName={eventMapImage.split("/").pop()} />
   </>
 );
 
